78c3e21101332f348a18d4750a150dbabc3b32fa
Ivan Maidana da Silveira <imadana30@gmail.com>
2022-05-01 19:44:46 -0300

    funcionou controladorbotoes.cpp e .h

--- T1/src/controladorBotoes.h
+++ T1/src/controladorBotoes.h
@@ -1,10 +1,40 @@
-#ifndef CONTROLADORBOTOES_H_INCLUDED
-#define CONTROLADORBOTOES_H_INCLUDED
+#ifndef GERENCIADOR_H_INCLUDED
+#define GERENCIADOR_H_INCLUDED
 
-class controladorBotoes
+#include <iostream>  //equivale ao stdio.h
+#include<stdio.h>
+
+#include"botao.h"
+#include "gl_canvas2d.h"
+#include"bmp.h"
+//#include "Mouse.h"
+
+class Gerenciador
 {
 private:
-    int test;
-};
+    Bmp *img01;
+    void desenhaBotoes();
+    void verificaMouseEmCima_botoes();
+    void verificaclick();
+    int getMouseState();
+    int getMouseY();
+    int getMouseX();
 
-#endif // CONTROLADORBOTOES_H_INCLUDED
+    int opcao = 50;             // variavel para selecao do que sera exibido na canvas.
+    // int mx, my;                //variaveis do mouse para poder exibir dentro da render().
+
+public:
+    Botao   *bt = NULL; //se a aplicacao tiver varios botoes, sugiro implementar um manager de botoes.
+    Botao   *bt1 = NULL;
+    Botao   *bt2 = NULL;
+    Botao   *bt3 = NULL;
+    int mouseX, mouseY, mouseState;
+    // Bmp* img;                // imagem a ser manipulada
+    int screenWidth, screenHeight;
+    Gerenciador();
+    void InitCanvas();
+    void Render(int mouse_x, int mouse_y, int mouse_state);
+    void Keyboard(int key);
+    void Mouse(int x, int y, int state);
+};
+#endif // GERENCIADOR_H_INCLUDED
--- T1/src/controladorbotoes.cpp
+++ T1/src/controladorbotoes.cpp
@@ -1,2 +1,96 @@
 #include"controladorBotoes.h"
 
+// Funçao contrutora da class Gerenciador responsavel por inicializar parametros
+Gerenciador :: Gerenciador()
+{
+    screenWidth = 700;
+    screenHeight = 700;
+}
+void Gerenciador::Render(int mouse_x, int mouse_y, int mouse_state)
+{
+    mouseX = mouse_x;
+    mouseY = mouse_y;
+    mouseState = mouse_state;
+
+    desenhaBotoes();
+    verificaMouseEmCima_botoes();
+    verificaclick();
+}
+
+void Gerenciador::desenhaBotoes()
+{
+    bt->Render();
+    bt1->Render();
+    bt2->Render();
+    bt3->Render();
+}
+
+int Gerenciador::getMouseState()
+{
+    return mouseState;
+}
+
+int Gerenciador::getMouseY()
+{
+    return mouseY;
+}
+
+int Gerenciador::getMouseX()
+{
+    return mouseX;
+}
+
+void Gerenciador::verificaMouseEmCima_botoes()
+{
+    if(bt->Colidiu( getMouseX(),  getMouseY()))
+    {
+        bt->controla_cor = 1;
+    }
+    else if(bt1->Colidiu( getMouseX(),  getMouseY()))
+    {
+        bt1->controla_cor = 1;
+    }
+    else if(bt2->Colidiu( getMouseX(),  getMouseY()))
+    {
+        bt2->controla_cor = 1;
+    }
+    else if(bt3->Colidiu( getMouseX(),  getMouseY()))
+    {
+        bt3->controla_cor = 1;
+    }
+}
+
+void Gerenciador::verificaclick()
+{
+    if(bt->Colidiu( getMouseX(),  getMouseY()) && getMouseState() == 0)
+    {
+        printf("clicou no primeiro botao\n");
+    }
+    else if(bt1->Colidiu( getMouseX(),  getMouseY()) && getMouseState()== 0)
+    {
+        printf("clicou no segundo botao\n");
+    }
+    else if(bt2->Colidiu( getMouseX(),  getMouseY()) && getMouseState() == 0)
+    {
+        printf("clicou no terceiro botao\n");
+    }
+    else if(bt3->Colidiu( getMouseX(),  getMouseY()) && getMouseState() == 0)
+    {
+        printf("clicou no quarto botao\n");
+    }
+}
+
+void Gerenciador::InitCanvas()
+{
+
+    //CV::init(&screenWidth, &screenHeight, "Titulo da Janela: Canvas 2D - Pressione 1, 2, 3");
+    //img01= new Bmp(".\\gl_1_canvasGlut\\resources\\img01.bmp");*
+    //img01->convertBGRtoRGB();
+
+    bt = new Botao(200, 400,  180, 80, "teste illuminati");
+    bt1 = new Botao(500, 480,  180, 80, "Sou um botao 2");
+    bt2 = new Botao(500, 380,  180, 80, "Sou um botao 3");
+    bt3 = new Botao(500, 280,  180, 80, "Sou um botao 4");
+
+    //CV::run();
+}
